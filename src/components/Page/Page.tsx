710531352f85aa1d3f9cc8d8de1492ad52b56168
Damon <mail@damonzucconi.com>
2019-12-12 08:46:36 -0500

    Fixes parsing of optional environment variables

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -81,10 +81,12 @@ const Entity = styled.div<{ size?: string | null }>`
 `;
 
 export const Page: React.FC = () => {
+  const pathname = window.location.pathname.slice(1);
+
   const [
     userId = process.env.REACT_APP_USER_ID,
     collectionId = process.env.REACT_APP_COLLECTION_ID
-  ] = window.location.pathname.slice(1).split("/");
+  ] = pathname === "" ? [] : pathname.split("/");
 
   const { data, loading, error } = useQuery<PageQuery, PageQueryVariables>(
     PAGE_QUERY,
